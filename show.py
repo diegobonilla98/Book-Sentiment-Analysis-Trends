51e2e675b735c168c0160c02db02009efc7d5837
Diego Bonilla <38797123+diegobonilla98@users.noreply.github.com>
2023-08-18 13:27:26 +0200

    Add files via upload

--- show.py
+++ show.py
@@ -1,28 +1,37 @@
 import pandas as pd
-from scipy.signal import savgol_filter
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-
+from statsmodels.nonparametric.smoothers_lowess import lowess
 
 file_path = "romeo_and_juliet.csv"
 df = pd.read_csv(file_path)
 df = df.drop(columns=["neutral"])
+
+# Apply Lowess smoothing to each column
 for col in df.columns:
-    df[col] = savgol_filter(df[col], 51, 3)
+    smoothed = lowess(df[col], np.arange(len(df[col])), frac=0.9)[:, 1]
+    df[col] = smoothed
 
 sns.set_style("whitegrid")
 emotions = df.columns[1:]
 palette = sns.color_palette("husl", len(emotions))
 fig, axs = plt.subplots(len(emotions), 1, figsize=(12, 4 * len(emotions)))
+
+# Calculate the global x-axis and y-axis limits
+global_xlim = (0, len(df) - 1)
+global_ylim = (df[emotions].min().min(), df[emotions].max().max())
+
 for ax, emotion, color in zip(axs, emotions, palette):
     ax.plot(df[emotion], color=color)
     ax.set_title(emotion, fontsize=16)
     ax.set_xlabel('Sentence Index', fontsize=14)
     ax.set_ylabel('Emotion Score', fontsize=14)
+    ax.set_xlim(global_xlim)
+    ax.set_ylim(global_ylim)
+
 plt.tight_layout()
 plt.savefig(file_path.replace(".csv", "_tight_plot.png"))
-
 
 N = 4
 variances = df.var().sort_values(ascending=False)
@@ -35,4 +44,6 @@ plt.title('Romeo and Juliet. Top {} Emotions Trend'.format(N), fontsize=18)
 plt.xlabel('Sentence Index', fontsize=14)
 plt.ylabel('Emotion Score', fontsize=14)
 plt.legend(loc='upper left', title="Emotions")
+plt.xlim(global_xlim)
+plt.ylim(global_ylim)
 plt.savefig(file_path.replace(".csv", "_join_plot.png"))
